767f5ec08d8051ba254ba8ca0ccce47940953197
MGYOSBEL <mgyosbel@gmail.com>
2020-12-18 16:16:18 +0100

    added lazy loading. could and shold be done with react.lazy() and Suspense

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import { Route, NavLink, Switch } from 'react-router-dom';
+import asyncComponent from '../../hoc/asyncComponent';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     render () {
@@ -32,7 +37,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/new-post" component={AsyncNewPost} />
                     <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
